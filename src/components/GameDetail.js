f5c76f91498aa7dd62f316f2acbc74153fd4ba28
aissamihi <aissamihi@gmail.com>
2021-10-17 19:00:30 -0400

    adding platforms images

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 //Styling and animation
 import styled from "styled-components";
 import { motion } from "framer-motion";
@@ -6,8 +6,17 @@ import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 
 import { useHistory } from "react-router-dom";
-
 import { smallImage } from "../util";
+
+//Images
+import apple from "../img/apple.svg";
+import gamepad from "../img/gamepad.svg";
+import nintendo from "../img/nintendo.svg";
+import playstation from "../img/playstation.svg";
+import stream from "../img/steam.svg";
+import xbox from "../img/xbox.svg";
+import emptyStar from "../img/star-empty.png";
+import fullStar from "../img/star-full.png";
 
 function GameDetail({ pathId }) {
   const history = useHistory();
@@ -21,10 +30,47 @@ function GameDetail({ pathId }) {
       history.push("/");
     }
   };
+
+  //Get platforms
+  const getPlatform = (platform) => {
+    if (platform.toLowerCase().includes("playstation".toLowerCase())) {
+      platform = "playstation";
+    } else if (platform.toLowerCase().includes("xbox".toLowerCase())) {
+      platform = "xbox";
+    } else if (platform.toLowerCase().includes("pc".toLowerCase())) {
+      platform = "pc";
+    } else if (platform.toLowerCase().includes("nintendo".toLowerCase())) {
+      platform = "nintendo";
+    } else if (platform.toLowerCase().includes("os".toLowerCase())) {
+      platform = "ios";
+    } else {
+      platform = "f";
+    }
+
+    switch (platform) {
+      case "playstation":
+        return playstation;
+      case "xbox":
+        return xbox;
+      case "pc":
+        return stream;
+      case "nintendo":
+        return nintendo;
+      case "ios":
+        return apple;
+      default:
+        return gamepad;
+    }
+  };
+  const [platformName, setPlatformName] = useState("");
+  const handleMouseOver = (name) => {
+    setPlatformName(name);
+  };
   //data
   const { game, screenshots, isLoading } = useSelector(
     (state) => state.details
   );
+
   return (
     <>
       {!isLoading && (
@@ -41,9 +87,23 @@ function GameDetail({ pathId }) {
               <Info>
                 <h3>Platforms</h3>
                 <Platforms>
-                  {game.platforms.map((data) => (
-                    <h3 key={data.platform.id}>{data.platform.name}</h3>
-                  ))}
+                  <div>
+                    {game.platforms.map((data) => (
+                      //<h3 key={data.platform.id}>{data.platform.name}</h3>
+                      <img
+                        key={data.platform.id}
+                        src={getPlatform(data.platform.name)}
+                        onMouseOver={() => handleMouseOver(data.platform.name)}
+                        onMouseLeave={() => {
+                          setPlatformName("");
+                        }}
+                        alt={data.platform.name}
+                      />
+                    ))}
+                  </div>
+                  <div className="platform-name">
+                    <p>{platformName}</p>
+                  </div>
                 </Platforms>
               </Info>
             </Stats>
@@ -118,15 +178,21 @@ const Info = styled(motion.div)`
 `;
 
 const Platforms = styled(motion.div)`
-  display: flex;
-  justify-content: space-evenly;
-  img {
-    margin-left: 3rem;
+  div {
+    display: flex;
+    justify-content: space-evenly;
+    img {
+      margin-left: 3rem;
+    }
+  }
+  .platform-name {
+    width: 100%;
+    height: 2rem;
   }
 `;
 
 const Media = styled(motion.div)`
-  margin-top: 5rem;
+  margin-top: 2rem;
   img {
     width: 100%;
   }
